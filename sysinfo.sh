41a6d8d18f5f5e0a88365c5402085e9c4a9822de
Contessa <contessarae11@gmail.com>
2024-02-21 17:45:27 +0000

    Update sysinfo.sh

---
+++ sysinfo.sh
@@ -0,0 +1,15 @@
+cd system_info
+
+{
+  echo "System information has been gathered. Here's the summary:"
+  echo "Date and Time: $(date '+%m/%d/%Y %H:%M:%S %Z')"
+  echo "Current User: $(whoami)"
+  echo "Current Working Directory: $(pwd)"
+  echo "System Usage:"
+  top -bn1 | head -5
+  echo "Disk Usage:"
+  df -h
+} > system_info.txt
+
+cat system_info.txt
+
